9280c18a4615ee6260065196ee01776d89703b6f
max77p <shanindrakumar@gmail.com>
2019-09-23 21:30:58 -0400

    update

--- firsthomework.py
+++ firsthomework.py
@@ -3,7 +3,16 @@ class Account:
     def __init__(self,name,amount):
         self.name=name 
         self.amount=amount 
-        print("created")
+        
+    def __str__(self):
+        return "Account owner: " + self.name + "\nAccount balance: " + str(self.amount)
+    
+    def deposit(self,dep):
+        self.amount=self.amount+dep
+        return str(self.amount)
+    def withdraw(self,dep):
+            self.amount=self.amount-dep
+            return str(self.amount)
 
 # 1. Instantiate the class
 acct1 = Account('Jose',100)
@@ -13,18 +22,18 @@ acct1 = Account('Jose',100)
 print(acct1)
 
 # # 3. Show the account owner attribute
-# acct1.owner
+print(acct1.name)
 
 
 # # 4. Show the account balance attribute
-# acct1.balance
+print(acct1.amount)
 
 
 # # 5. Make a series of deposits and withdrawals
-# acct1.deposit(50)
+print(acct1.deposit(50))
 
 
-# acct1.withdraw(75)
+print(acct1.withdraw(75))
 
 
 # # 6. Make a withdrawal that exceeds the available balance
